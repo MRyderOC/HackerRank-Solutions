58835be4a96d6861f0ab2edc50eec9b6937e4c53
Milad <63479762+MRyderOC@users.noreply.github.com>
2021-09-06 01:49:44 -0500

    Data Structures part and array folder added

---
+++ Data Structures/Arrays/Arrays - DS.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    # Old school approach
+    l = []
+    for i in range(len(a)-1, -1, -1):
+        l.append(a[i])
+    return l
+    # Old school list comprehension approach
+    return [a[i] for i in range(len(a)-1, -1, -1)]
+    # Built-in approach
+    return reversed(a)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
