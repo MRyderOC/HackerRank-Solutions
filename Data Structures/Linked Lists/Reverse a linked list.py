43a0002bfb01d9d2efb93d171e3e91e1b1e9eb0c
Milad <63479762+MRyderOC@users.noreply.github.com>
2021-09-13 11:33:46 -0500

    Two problems added to Linked Lists

---
+++ Data Structures/Linked Lists/Reverse a linked list.py
@@ -0,0 +1,98 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'reverse' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def reverse(llist):
+    # 0-node, 1-node Linked List
+    if llist is None or llist.next is None:
+        return llist
+    # 2-node Linked List
+    if llist.next.next is None:
+        tmp = llist.next
+        tmp.next = llist
+        llist.next = None
+        return tmp
+    # more than 2-node Linked List
+    p1 = None
+    p2 = llist
+    p3 = p2.next
+    while p3 is not None:
+        p2.next = p1
+        p1 = p2
+        p2 = p3
+        p3 = p3.next
+    p2.next = p1
+    return p2
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_singly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
+
