3ee03bd1e86a301c45e3b7ea9fcdcbf49422eac8
Milad <63479762+MRyderOC@users.noreply.github.com>
2021-10-04 23:08:00 -0500

    A stack problem added

---
+++ Problem Solving/Data Structures/Stacks/Equal Stacks.py
@@ -0,0 +1,56 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalStacks' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h1
+#  2. INTEGER_ARRAY h2
+#  3. INTEGER_ARRAY h3
+#
+from itertools import accumulate
+def equalStacks(h1, h2, h3):
+    tmp1 = set(accumulate(h1[::-1]))
+    tmp2 = set(accumulate(h2[::-1]))
+    tmp3 = set(accumulate(h3[::-1]))
+    intersections = tmp1.intersection(tmp2).intersection(tmp3)
+    if intersections:
+        return max(intersections)
+    else:
+        return 0
+    
+    
+    
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n1 = int(first_multiple_input[0])
+
+    n2 = int(first_multiple_input[1])
+
+    n3 = int(first_multiple_input[2])
+
+    h1 = list(map(int, input().rstrip().split()))
+
+    h2 = list(map(int, input().rstrip().split()))
+
+    h3 = list(map(int, input().rstrip().split()))
+
+    result = equalStacks(h1, h2, h3)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
