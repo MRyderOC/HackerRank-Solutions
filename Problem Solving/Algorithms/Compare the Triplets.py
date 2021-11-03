c89342f3db1a049666b55e748572f1ae5e20acbd
Milad <63479762+MRyderOC@users.noreply.github.com>
2021-11-02 23:47:38 -0500

    Two warmup problems added

---
+++ Problem Solving/Algorithms/Compare the Triplets.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    score_a, score_b = 0, 0
+    for i, j in zip(a, b):
+        if i > j:
+            score_a += 1
+        elif i < j:
+            score_b += 1
+        else:
+            continue
+    return [score_a, score_b]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
