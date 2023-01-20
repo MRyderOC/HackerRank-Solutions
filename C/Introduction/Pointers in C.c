760d49021e81887db95cafce9d2ca80b3728dc57
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-01-20 01:47:49 -0600

    Pointers in C problem added

---
+++ C/Introduction/Pointers in C.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    int s = *a + *b;
+    int d = abs(*a - *b);
+    *a = s;
+    *b = d;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
