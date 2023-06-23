a12fa8a21524a0917757be5ae415e30f996a50f3
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-06-22 21:00:59 -0500

    Army Game problem added for Python

---
+++ Mathematics/Fundamentals/Army Game.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gameWithCells' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def gameWithCells(n, m):
+    # Write your code here
+    n2, m2 = n // 2, m // 2
+    n_m, m_m = n % 2, m % 2
+    return (n2 * m2) + (m_m * n2 + n_m * m2) + (n_m * m_m)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = gameWithCells(n, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
