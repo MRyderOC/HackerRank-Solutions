1548a575b7409a747c0ec4f5e2bb30f15c1c6d68
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-06-26 19:23:17 -0500

    lint fix for Array Manipulation for Python

--- Problem Solving/Data Structures/Arrays/Array Manipulation.py
+++ Problem Solving/Data Structures/Arrays/Array Manipulation.py
@@ -20,12 +20,12 @@ import array
 def arrayManipulation(n, queries):
     arr = array.array('q', [0] * n)
     for q in queries:
-        a, b, k = q[0]-1, q[1]-1, q[2]
+        a, b, k = q[0] - 1, q[1] - 1, q[2]
         arr[a] += k
-        if b != n-1:
-            arr[b+1] -= k
+        if b != n - 1:
+            arr[b + 1] -= k
     return max(accumulate(arr))
-        
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
