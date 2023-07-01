63f35d3f7dc6bc2d60d8018a385604c70bbeb2f4
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-06-30 20:42:05 -0500

    Compare two linked lists problem updated for Python

--- Problem Solving/Data Structures/Linked Lists/Compare two linked lists.py
+++ Problem Solving/Data Structures/Linked Lists/Compare two linked lists.py
@@ -46,21 +46,23 @@ def print_singly_linked_list(node, sep, fptr):
 def compare_lists(llist1, llist2):
     if llist1 is None and llist2 is None:
         return 1
-    
-    flag = True
+
     while True:
-        if llist1.data == llist2.data:
-            if llist1.next is not None and llist2.next is not None:
-                llist1 = llist1.next
-                llist2 = llist2.next
-            elif (llist1.next is None and llist2.next is not None)\
-            or (llist1.next is not None and llist2.next is None):
-                return 0
-            else:
-                return 1
-        else:
+        if llist1.data != llist2.data:
             return 0
-        
+
+        if (
+            (llist1.next is not None and llist2.next is None) or
+            (llist1.next is None and llist2.next is not None)
+        ):
+            return 0
+
+        if llist1.next is None and llist2.next is None:
+            return 1
+
+        llist1 = llist1.next
+        llist2 = llist2.next
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
