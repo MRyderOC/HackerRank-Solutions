6a5d0752a39e4f83c013155c3fffc6bf4d23a0bd
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-07-27 20:21:51 -0500

    Capturing & Non-Capturing Groups problem added for C++

---
+++ Regex/Grouping and Capturing/Capturing & Non-Capturing Groups.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <regex>
+
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    regex regex_pattern {"(ok){3,}"};
+    string s;
+    getline(cin, s);
+    cout << boolalpha << regex_search(s, regex_pattern);
+
+    return 0;
+}
