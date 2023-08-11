0d3d35daa79a17ade6a69a0292a19fe676352ff4
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-08-11 16:16:17 -0500

    Find HackerRank problem added for Python

---
+++ Regex/Applications/Find HackerRank.py
@@ -0,0 +1,20 @@
+import re
+
+start_pattern = r"^hackerrank.*"
+end_pattern = r".*hackerrank$"
+start_end_pattern = r"^hackerrank(.*)hackerrank$"
+
+n = int(input())
+for _ in range(n):
+    row_input = input()
+    start_match = re.search(start_pattern, row_input)
+    end_match = re.search(end_pattern, row_input)
+
+    if start_match and end_match:
+        print(0)
+    elif end_match:
+        print(2)
+    elif start_match:
+        print(1)
+    else:
+        print(-1)
