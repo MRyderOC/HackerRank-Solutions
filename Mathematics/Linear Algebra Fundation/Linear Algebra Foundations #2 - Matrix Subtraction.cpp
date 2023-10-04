843b391fad9deac84ba20dd5f54b62e1d88d7bf3
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-10-03 22:21:23 -0500

    Linear Algebra Foundations #2 - Matrix Subtraction problem added for C++

---
+++ Mathematics/Linear Algebra Fundation/Linear Algebra Foundations #2 - Matrix Subtraction.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    cout << "-3\n-3\n-3\n-5\n-5\n-5\n-3\n-4\n1";
+    return 0;
+}
