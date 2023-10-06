9cb2ac6e7f63716335cf7b8b15fc6f9b9fa19b29
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-10-05 20:37:08 -0500

    Linear Algebra Foundations #4- Matrix Multiplication problem added for C++

---
+++ Mathematics/Linear Algebra Fundation/Linear Algebra Foundations #4- Matrix Multiplication.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    cout << "30\n36\n45\n45\n54\n67\n15\n18\n22";
+    return 0;
+}
