48b50c124630cfac6f0278e4af337c41e4492980
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-10-09 17:59:27 -0500

    Linear Algebra Foundations #7 - The 1000th Power of a Matrix problem added for Python

---
+++ Mathematics/Linear Algebra Fundation/Linear Algebra Foundations #7 - The 1000th Power of a Matrix.py
@@ -0,0 +1,9 @@
+# import numpy as np
+
+# x = np.array([[-2, -9], [1, 4]])
+# y = np.linalg.matrix_power(x, 1000)
+
+# out = "\n".join(list(map(str, list(map(int, list(y.flatten()))))))
+# print(out)
+
+print("-2999\n-9000\n1000\n3001")
