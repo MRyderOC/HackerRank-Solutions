0d2e376398383dfb0d67c611e1c4cb01e7c2328e
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-10-09 18:00:13 -0500

    Linear Algebra Foundations #7 - The 1000th Power of a Matrix problem added for C++

---
+++ Mathematics/Linear Algebra Fundation/Linear Algebra Foundations #7 - The 1000th Power of a Matrix.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    cout << "-2999\n-9000\n1000\n3001";
+    return 0;
+}
