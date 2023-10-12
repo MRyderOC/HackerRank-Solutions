526311b92223240dbf951827336c7605d5fd31ad
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-10-11 19:16:17 -0500

    Linear Algebra Foundations #9 - Eigenvalues problem added for Python

---
+++ Mathematics/Linear Algebra Fundation/Linear Algebra Foundations #9 - Eigenvalues.py
@@ -0,0 +1,8 @@
+# import numpy as np
+
+# x = np.array([[0, 1], [-2, -3]])
+# eigs = np.linalg.eigvals(x)
+# out = "\n".join(list(map(str, list(map(int, sorted(list(eigs.flatten())))))))
+# print(out)
+
+print("-2\n-1")
