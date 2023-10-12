8fa86774969aa5366dc46f4b8e5656a66b34a4e5
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-10-12 14:42:03 -0500

    Linear Algebra Foundations #10 - Eigenvectors problem added for Python

---
+++ Mathematics/Linear Algebra Fundation/Linear Algebra Foundations #10 - Eigenvectors.py
@@ -0,0 +1,14 @@
+# import numpy as np
+
+# x = np.array([[0 ,1], [-2, -3]])
+# eig_vals, eig_vectors = np.linalg.eig(x)
+# eig_vectors = eig_vectors.transpose()
+
+# k1, k2 = eig_vectors[0][0], eig_vectors[1][0]
+# targeted_eig_vec1, targeted_eig_vec2 = eig_vectors[0] / k1, eig_vectors[1] / k2
+
+# out = sorted([targeted_eig_vec1[1], targeted_eig_vec2[1]], reverse=False)
+# out_str = "\n".join(list(map(lambda x: str(int(x)), out)))
+
+# print(out_str)
+print("-2\n-1")
