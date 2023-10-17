62ceeac028eea79eb39e16dfd5244f74f2649908
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-10-17 15:56:16 -0500

    Eigenvalue of matrix #2 problem added for Python

---
+++ Mathematics/Linear Algebra Fundation/Eigenvalue of matrix #2.py
@@ -0,0 +1,10 @@
+# import numpy as np
+
+# x = np.array([[1, 2], [2, 4]])
+# eigs = np.linalg.eigvals(x)
+
+# out = "\n".join(list(map(str, list(map(int, sorted(list(eigs.flatten())))))))
+
+# print(out)
+
+print("0\n5")
