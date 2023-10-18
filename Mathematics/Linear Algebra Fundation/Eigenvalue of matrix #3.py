17d9baaa26e609b93d8b4957d446697f1a79ae8e
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-10-18 11:36:09 -0500

    Eigenvalue of matrix #3 problem added for Python

---
+++ Mathematics/Linear Algebra Fundation/Eigenvalue of matrix #3.py
@@ -0,0 +1,14 @@
+# import numpy as np
+
+# A = np.array([[2, -1], [-1, 2]])
+# A_squared = np.linalg.matrix_power(A, 2)
+
+# eigs_A = np.linalg.eigvals(A)
+# eigs_A2 = np.linalg.eigvals(A_squared)
+# eigs = sorted(list(eigs_A.flatten())) + sorted(list(eigs_A2.flatten()))
+
+# float_to_int_to_str = lambda x: str(int(x))
+# out = "\n".join(list(map(float_to_int_to_str, eigs)))
+# print(out)
+
+print("1\n3\n1\n9")
