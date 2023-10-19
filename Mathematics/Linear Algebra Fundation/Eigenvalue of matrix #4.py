d05483671a9301a8426afca2f88030551b87705a
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-10-19 13:27:15 -0500

    Eigenvalue of matrix #4 problem added for Python

---
+++ Mathematics/Linear Algebra Fundation/Eigenvalue of matrix #4.py
@@ -0,0 +1,15 @@
+# import numpy as np
+
+# A = np.array([[2, -1], [-1, 2]])
+# A_inverse = np.linalg.inv(A)
+# A_4I = A - 4 * np.identity(2)
+
+# eigs_A_inv = np.linalg.eigvals(A_inverse)
+# eigs_A_4I = np.linalg.eigvals(A_4I)
+
+# eigs = sorted(list(eigs_A_inv.flatten())) + sorted(list(eigs_A_4I.flatten()))
+
+# out = "\n".join(eigs)
+# print(out)
+
+print("1/3\n1\n-3\n-1")
