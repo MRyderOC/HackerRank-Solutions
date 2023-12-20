787d68f7fc53e75688a02249018a4328f6dec5ed
MRyderOC <milad.tabrizi@gmail.com>
2023-12-20 11:34:44 -0600

    Add Viral Advertising problem for Python

---
+++ Problem Solving/Algorithms/Implementation/Viral Advertising.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    total_like = 0
+    reached = 5
+    for i in range(n):
+        liked = reached // 2
+        reached = liked * 3
+        total_like += liked
+
+    return total_like
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
