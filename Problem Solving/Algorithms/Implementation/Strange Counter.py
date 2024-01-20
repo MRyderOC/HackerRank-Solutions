3a59788af32a5f4ce1a2b66e2e6738a7917f2933
Milad <63479762+MRyderOC@users.noreply.github.com>
2024-01-19 19:31:51 -0600

    Add Strange Counter problem for Python

---
+++ Problem Solving/Algorithms/Implementation/Strange Counter.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'strangeCounter' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER t as parameter.
+#
+
+def strangeCounter(t):
+    # Write your code here
+    x = 3
+    while True:
+        if t // x == 0:
+            break
+        t -= x
+        x *= 2
+
+    if t == 0:
+        return 1
+    return x - t + 1
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    result = strangeCounter(t)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
