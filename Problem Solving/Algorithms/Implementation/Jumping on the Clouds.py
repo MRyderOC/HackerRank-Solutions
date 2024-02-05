82d26b00053f5f9f2cae71ad1a6000235fdd855b
Milad <63479762+MRyderOC@users.noreply.github.com>
2024-02-04 22:28:16 -0600

    Add Jumping on the Clouds problem for Python

---
+++ Problem Solving/Algorithms/Implementation/Jumping on the Clouds.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    # Write your code here
+    count = 0
+    i = 0
+    n = len(c)
+    while i < n - 1:
+        if i == n - 2:
+            count += 1
+            break
+        if c[i + 2] == 0:
+            i += 2
+        else:
+            i += 1
+        count += 1
+
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
