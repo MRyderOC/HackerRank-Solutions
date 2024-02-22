a9602203ef30beb160f6845880a2c99725fb2815
MRyderOC <milad.tabrizi@gmail.com>
2024-02-22 11:14:53 -0600

    Add Intro to Tutorial Challenges problem for Python

---
+++ Problem Solving/Algorithms/Sorting/Intro to Tutorial Challenges.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import bisect
+
+#
+# Complete the 'introTutorial' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER V
+#  2. INTEGER_ARRAY arr
+#
+
+def introTutorial(V, arr):
+    # Write your code here
+    i = bisect.bisect_left(arr, V)
+    if arr[i] == V and i != len(arr):
+        return i
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    V = int(input().strip())
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = introTutorial(V, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
