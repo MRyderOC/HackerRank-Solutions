7e05816dbb7756b43600d53012f51129f5d28273
MRyderOC <milad.tabrizi@gmail.com>
2024-05-10 18:21:58 -0500

    Add The Power Sum problem for C++

---
+++ Problem Solving/Algorithms/Recursion/The Power Sum.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#include <math.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'powerSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER X
+ *  2. INTEGER N
+ */
+
+int powerSumHelper(int X, int N, int init) {
+    if (pow(init, N) > X)
+        return 0;
+    else if (pow(init, N) == X)
+        return 1;
+    else
+        return powerSumHelper(X, N, init + 1) + powerSumHelper(X - pow(init, N), N, init + 1);
+}
+
+int powerSum(int X, int N) {
+    return powerSumHelper(X, N, 1);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string X_temp;
+    getline(cin, X_temp);
+
+    int X = stoi(ltrim(rtrim(X_temp)));
+
+    string N_temp;
+    getline(cin, N_temp);
+
+    int N = stoi(ltrim(rtrim(N_temp)));
+
+    int result = powerSum(X, N);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
